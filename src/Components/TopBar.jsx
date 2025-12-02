7e86ea567b2b2b4f3184d7e856524734f0ebbe08
Hlovegood <hlovegood14@gmail.com>
2025-12-02 19:27:14 +0200

    TopBar Finished

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -4,14 +4,14 @@ import BellIcon from '../assets/Icons/Bell Icon.svg';
 import SettingsIcon from '../assets/Icons/Settings Icon.svg';
 const TopBar = () => {
     return ( <>
-    <section>
-    <article>
-        <div>
-            <img src={BellIcon} alt="Notification Bell Icon" />
+    <section className='TopBar'>
+    <article className='TopBar-Content'>
+        <div className='IconHolder'>
+            <img src={BellIcon} alt="Notification Bell Icon" className='Icon' />
         </div>
 
-        <div>
-            <img src={SettingsIcon} alt="Settings Icon" />
+        <div className='IconHolder'>
+            <img src={SettingsIcon} alt="Settings Icon"  className='Icon'/>
         </div>
     </article>
 
