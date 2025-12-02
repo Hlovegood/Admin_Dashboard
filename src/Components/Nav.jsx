67d642d82603ff1a1bfeb74983ee74076e7d3cf7
Hlovegood <hlovegood14@gmail.com>
2025-12-02 12:27:02 +0200

    Projects&Users  Icon in Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import  NavLink  from '../common/NavLink';
 import './Nav.css'
 import DashIcon from '../assets/Icons/Dashboard Icon.svg';
+import ProjectsIcon from '../assets/Icons/Projects Icon.svg';
+import UsersIcon from '../assets/Icons/Users Icon.svg'
 const Nav = () => {
     return ( <>
     <nav>
         <ul>
             <li>
+                <Link>
                 <NavLink Icon={DashIcon} Text={"Dashboard"}/>
+                </Link>
             </li>
             
+            <li>
+                <Link>
+                <NavLink Icon={ProjectsIcon} Text={"Projects"}/>
+                </Link>
+            </li>
+
+            <li>
+                <Link>
+                <NavLink Icon={UsersIcon} Text={"Users"}/>
+                </Link>
+            </li>
         </ul>
     </nav>
     
